9753eb0947eaf5af85c8587b0433925b35a303b2
Yordan Caicedo <yordane.caicedoc@uqvirtual.edu.co>
2022-03-19 16:19:07 -0500

    Entidades de la biblioteca y gitignore

--- persistencia/src/main/java/co/edu/uniquindio/unitravel/PersistenciaApplication.java
+++ persistencia/src/main/java/co/edu/uniquindio/unitravel/PersistenciaApplication.java
@@ -3,11 +3,13 @@ package co.edu.uniquindio.unitravel;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class PersistenciaApplication {
 
     public static void main(String[] args) {
-    SpringApplication.run(PersistenciaApplication.class, args);
+       SpringApplication.run(PersistenciaApplication.class, args);
+
 
     }
 
---
+++ persistencia/src/main/java/co/edu/uniquindio/unitravel/entidades/Autor.java
@@ -0,0 +1,5 @@
+package co.edu.uniquindio.unitravel.entidades;
+
+
+public class Autor {
+}
